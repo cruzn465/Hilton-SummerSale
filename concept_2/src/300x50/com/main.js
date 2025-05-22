15af1a7266b0c3cb866b3cd487ef8428b5ec708b
nichole <nichole.cruzalegui@bbdo.com>
2025-05-22 15:14:22 -0400

    Starts to update for simple aniamtion

--- concept_2/src/300x50/com/main.js
+++ concept_2/src/300x50/com/main.js
@@ -77,7 +77,7 @@ function animate(){
 
     // LOGO ANIMATION
     .from(logo_2x, 1.6, {opacity: 0, ease: Power1.easeInOut},0)
-    .from(logoCopy_2x, .4, {y:10,opacity: 0, ease: Power1.easeInOut},"-=.2")
+    .from(logoCopy, .4, {y:10,opacity: 0, ease: Power1.easeInOut},"-=.2")
     .from(circ5_2x, .3, {opacity: 0, ease: Power1.easeInOut},"-=.2")
     
     // COPY ANIMATION
